327194e4b2a5f14a62f5223e4f9b948290cd0215
Tano Paul <tanopaul38@gmail.com>
2023-08-02 16:46:47 -0400

    completed array find method

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // code your solution here
+
+const record = [
+    {year: '2014', result: 'N/A'},
+    {year: '2013', result: 'L'},
+    {year: '2015', result: 'W'}
+]
+
+function superbowlWin(arr) {
+   
+    let foundItem = arr.find(word => {
+        if (word.result === 'W') {
+            return word;
+        } else {
+            return undefined;
+        }
+    })
+
+    
+
+    if (typeof foundItem === 'object') {
+        let answer = foundItem.year;
+        console.log(answer)
+        return answer;
+    } 
+    console.log(typeof foundItem)
+    return undefined;
+    
+    
+    
+}
+
+
+
+superbowlWin(record)
+
+
